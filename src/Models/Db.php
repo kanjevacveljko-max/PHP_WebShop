51fa1b597b88fe63a92f3a18d6e70ded2eb02410
Veljko <kanjevacveljko@gmail.com>
2025-09-23 18:02:36 +0200

    Made connection to database

---
+++ src/Models/Db.php
@@ -0,0 +1,10 @@
+<?php
+
+class Db{
+
+    public $connection;
+
+    public function __construct(){
+        $this->connection = new PDO('mysql:host=localhost;dbname=webshop', 'root', '');
+    }
+}
